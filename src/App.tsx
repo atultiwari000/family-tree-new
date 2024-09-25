97eeb1dd291c4624d84b23c196f3eebcea702911
EV-OD <evod599@gmail.com>
2024-09-25 16:06:29 +0545

    added images, and toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
 import React from 'react';
 import './App.css';
 import FamilyTreeComponent from './component/FamilyTree';
+import Ambika from "@/assets/images/ambika.png"
+import hariMaya from "@/assets/images/hari-maya.png"
+
+import bidhya from "@/assets/images/bidhya.png"
+import gita from "@/assets/images/gita.png"
+import hariKrishna from "@/assets/images/hari-krishna.png"
+import keshab from "@/assets/images/keshab.png"
+import malati from "@/assets/images/malati.png"
+import maya from "@/assets/images/maya.png"
+import me from "@/assets/images/me.png"
+import mummy from "@/assets/images/mummy.png"
+import rikhi from "@/assets/images/rikhi.png"
+import subhash from "@/assets/images/subhash.png"
+import sarswati from "@/assets/images/sarswati.png"
+import avatar from "@/assets/images/avatar.png"
+
+
+
 
 const App: React.FC = () => {
   const nodes = [
-    {id: 1, name:"Rikhi Ram Lc",pids:[2], gender:"male", img:"./images/rikhi.png"},
-    {id: 2, name:"Hari Maya Lc", pids:[1], gender:"female", img:"./images/hari-maya.png"},
+    {id: 1, name:"Rikhi Ram Lc",pids:[2], gender:"male", img:rikhi},
+    {id: 2, name:"Hari Maya Lc", pids:[1], gender:"female", img:hariMaya},
 
     // sons
-    {id: 3, name:"Keshab Raj Lc", mid:[1], fid:[2], pids:[10], gender:"male", img:"./images/keshab.png"},
-    {id: 4, name:"kshitiz Lc", mid:[1], fid:[2], pids:[11], gender:"male", img:"./images/avatar.png"},
-    {id: 5, name:"Hari Krishna Lc", mid:[1], fid:[2] ,pids:[12], gender:"male", img:"./images/hari-krishna.png"},
+    {id: 3, name:"Keshab Raj Lc", mid:[1], fid:[2], pids:[10], gender:"male", img:keshab},
+    {id: 4, name:"kshitiz Lc", mid:[1], fid:[2], pids:[11], gender:"male", img:avatar},
+    {id: 5, name:"Hari Krishna Lc", mid:[1], fid:[2] ,pids:[12], gender:"male", img:hariKrishna},
 
     // daughters
-    {id: 6, name:"Sarswati Subedi", mid:[1], fid:[2], gender:"female", img:"./images/sarswati.png"},
-    {id: 7, name:"Malati Tiwari", mid:[1], fid:[2], gender:"female", img:"./images/malati.png"},
-    {id: 8, name:"Ambika Lc", mid:[1], fid:[2], gender:"female", img:"./images/ambika.png"},
-    {id: 9, name:"Maya Bhattrai", mid:[1], fid:[2], gender:"female", img:"./images/maya.png"},
+    {id: 6, name:"Sarswati Subedi", mid:[1], fid:[2], gender:"female", img:sarswati},
+    {id: 7, name:"Malati Tiwari", mid:[1], fid:[2], gender:"female", img:malati},
+    {id: 8, name:"Ambika Lc", mid:[1], fid:[2], gender:"female", img:Ambika},
+    {id: 9, name:"Maya Bhattrai", mid:[1], fid:[2], gender:"female", img:maya},
 
     // wife of sons
-    {id: 10, name:"Sarswati Kumari Lc", pids:[3], gender:"female", img:"./images/mummy.png"},
-    {id: 11, name:"Bidhya Chettri", pids:[4], gender:"female", img:"./images/bidhya.png"},
-    {id: 12, name:"Gita Lc", pids:[5], gender:"female", img:"./images/gita.png"},
+    {id: 10, name:"Sarswati Kumari Lc", pids:[3], gender:"female", img:mummy},
+    {id: 11, name:"Bidhya Chettri", pids:[4], gender:"female", img:bidhya},
+    {id: 12, name:"Gita Lc", pids:[5], gender:"female", img:gita},
 
     // children of sons
-    {id: 13, name:"Subhash Lc" , fid:[3], mid:[10], gender:"male", img:"./images/subhash.png"},
-    {id: 14, name:"Rabin Lc" , fid:[3], mid:[10], gender:"male", img:"./images/me.png"},
+    {id: 13, name:"Subhash Lc" , fid:[3], mid:[10], gender:"male", img:subhash},
+    {id: 14, name:"Rabin Lc" , fid:[3], mid:[10], gender:"male", img:me},
 
 ]
 
--- src/component/FamilyTree.tsx
+++ src/component/FamilyTree.tsx
@@ -1,84 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import  FamilyTree  from '@/family-tree/family-tree.js';
-
-interface FamilyTreeProps {
-  nodes: {
-    [key: string]: any;
-  }[];
-}
-
-FamilyTree.templates.rabin = Object.assign({}, FamilyTree.templates.base);
-FamilyTree.templates.rabin.defs = `<style>
-                                    .{randId} .bft-edit-form-header, .{randId} .bft-img-button{
-                                        background-color: #aeaeae;
-                                    }
-                                    .{randId}.male .bft-edit-form-header, .{randId}.male .bft-img-button{
-                                        background-color: #039BE5;
-                                    }        
-                                    .{randId}.male div.bft-img-button:hover{
-                                        background-color: #F57C00;
-                                    }
-                                    .{randId}.female .bft-edit-form-header, .{randId}.female .bft-img-button{
-                                        background-color: #F57C00;
-                                    }        
-                                    .{randId}.female div.bft-img-button:hover{
-                                        background-color: #039BE5;
-                                    }
-                                </style>
-                                <clipPath id="rabin_img_0"><rect x="6" y="6" rx="54" ry="54" width="108" height="108"></rect></clipPath>
-                                ${FamilyTree.gradientCircleForDefs('circle', '#aeaeae', 60, 5)}
-                                ${FamilyTree.gradientCircleForDefs('male_circle', '#039BE5', 60, 5)}
-                                ${FamilyTree.gradientCircleForDefs('female_circle', '#F57C00', 60, 5)}`;
-FamilyTree.templates.rabin.field_0 = 
-    '<text ' + FamilyTree.attr.width + ' ="230" class="user_name" style="font-size: 16px;font-weight:bold;" fill="#aeaeae" x="60" y="145" text-anchor="middle">{val}</text>';
-    // FamilyTree.templates.rabin.field_0 = '<text style="font-size: 24px;" fill="#ffffff" x="100" y="90" text-anchor="middle">{val}</text>';
-
-{/* <switch>
-<foreignObject x="20" y="90" FamilyTree.attr.width="150" height="200">
-<p xmlns="http://www.w3.org/1999/xhtml">Text goes here</p>
-</foreignObject>
-
-<text x="20" y="20">Your SVG viewer cannot display html.</text>
-</switch> */}
-// FamilyTree.templates.rabin.field_0 = 
-//     `
-//     <switch>
-//     <foreignObject x="60" y="135" width="230" height="50">
-//     <p xmlns="http://www.w3.org/1999/xhtml" style="font-size: 16px;font-weight:bold;" fill="#aeaeae; te">{val}</p>
-//     </foreignObject>
-
-//     <text x="20" y="20">Your SVG viewer cannot display html.</text>
-//     </switch>
-// `;
-
-{/* <text ' + FamilyTree.attr.width + ' ="230" style="font-size: 16px;font-weight:bold;" fill="#aeaeae" x="60" y="135" text-anchor="middle">{val}</text> */}
-FamilyTree.templates.rabin.field_1 = 
-    '<text ' + FamilyTree.attr.width + ' ="150" style="font-size: 13px;" fill="#aeaeae" x="60" y="150" text-anchor="middle">{val}</text>';
-FamilyTree.templates.rabin.node = '<use x="0" y="0" xlink:href="#circle" />';
-FamilyTree.templates.rabin.img_0 = 
-    '<image preserveAspectRatio="xMidYMid slice" clip-path="url(#rabin_img_0)" xlink:href="{val}" x="6" y="6" width="108" height="108"></image>';
-FamilyTree.templates.rabin.ripple = {
-    radius: 60,
-    color: "#e6e6e6",
-    rect: undefined
-};
-
-FamilyTree.templates.rabin.size = [120, 120]
-FamilyTree.templates.rabin_male = Object.assign({}, FamilyTree.templates.rabin);
-FamilyTree.templates.rabin_male.node += '<use x="0" y="0" xlink:href="#male_circle" />';
-FamilyTree.templates.rabin_male.ripple = {
-    radius: 60,
-    color: "#039BE5",
-    rect: undefined
-};
-FamilyTree.templates.rabin_female = Object.assign({}, FamilyTree.templates.rabin);
-FamilyTree.templates.rabin_female.node += '<use x="0" y="0" xlink:href="#female_circle" />';
-FamilyTree.templates.rabin_female.ripple = {
-    radius: 60,
-    color: "#F57C00",
-    rect: undefined
-};
-FamilyTree.templates.rabin.nodeMenuButton = `<use ${FamilyTree.attr.control_node_menu_id}="{id}" x="90" y="50" xlink:href="#base_node_menu" />`;
+import FamilyTree, { FamilyTreeProps } from "@/family-tree/wrapper"
 
 const FamilyTreeComponent: React.FC<FamilyTreeProps> = ({ nodes }) => {
   const divRef = useRef<HTMLDivElement>(null);
@@ -96,11 +17,11 @@ const FamilyTreeComponent: React.FC<FamilyTreeProps> = ({ nodes }) => {
         levelSeparation: 100,
         siblingSeparation: 50,
         subtreeSeparation: 100,
+        toolbar: {
+          zoom: true,
+          fit: true,
+        },
       });
-
-
-      console.log('Family tree instance:', family);
-      console.log('Nodes:', nodes);
     }
   }, [nodes]);
 
