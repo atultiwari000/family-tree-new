6c1c81a1b9c85ffc5b220831d033fbb005f25245
atultiwari000 <tiwari.000atul@gmail.com>
2024-10-06 15:03:59 +0545

    added member button

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,8 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGfpz6pgp4N-twDhbNjdlJLExxiH2TK58",
-  authDomain: "family-tree-72775.firebaseapp.com",
-  projectId: "family-tree-72775",
-  storageBucket: "family-tree-72775.appspot.com",
-  messagingSenderId: "527666511066",
-  appId: "1:527666511066:web:c694dfadfbcb754a0d39e3"
+  //need to add config
+  
 };
 
 const app = initializeApp(firebaseConfig);
