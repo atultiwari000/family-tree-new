2d137938df5c65fb280bb8aaaa5ae89d93dc9012
atultiwari000 <tiwari.000atul@gmail.com>
2024-10-12 15:56:39 +0545

    fixed little bugs

--- src/components/login-ui.tsx
+++ src/components/login-ui.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react';
-import {  signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
-import {Button} from '@/components/ui/button';
-import {Avatar, AvatarImage, AvatarFallback} from '@/components/ui/avatar';
+import React, { useEffect } from "react";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
-import { auth} from '@/firebase';
-import useFamilyStore from '@/store/globalFamily';
-
-
-
+import { auth } from "@/firebase";
+import useFamilyStore from "@/store/globalFamily";
 
 // Initialize Firebase
 const provider = new GoogleAuthProvider();
 
 const LoginUI: React.FC = () => {
-  const user =  useFamilyStore((state) => state.user);
+  const user = useFamilyStore((state) => state.user);
   const setUser = useFamilyStore((state) => state.setUser);
 
   useEffect(() => {
@@ -41,17 +42,15 @@ const LoginUI: React.FC = () => {
   };
 
   return (
-    <div className="absolute z-10 m-3 mt-32">
+    <div className="absolute z-10 m-3 mt-5">
       {user ? (
         <div>
-          <div className='flex items-center gap-3 mb-3'>
-          <Avatar>
-            <AvatarImage src={user.photoURL} alt="user avatar" />
-            <AvatarFallback>
-              {user.displayName[0]}
-            </AvatarFallback>
-          </Avatar>
-          <h6>{user.displayName}</h6>
+          <div className="flex items-center gap-3 mb-3">
+            <Avatar>
+              <AvatarImage src={user.photoURL} alt="user avatar" />
+              <AvatarFallback>{user.displayName[0]}</AvatarFallback>
+            </Avatar>
+            <h6>{user.displayName}</h6>
           </div>
           <Button onClick={handleLogout}>Logout</Button>
         </div>
--- src/custom-component/FamilyTreeComponent.tsx
+++ src/custom-component/FamilyTreeComponent.tsx
@@ -13,7 +13,8 @@ import {
 import { FamilyMember } from "@/types/familyTypes";
 import { DebugInfo } from "./DebugInfo";
 import useFamilyStore from "@/store/globalFamily";
-import { GearIcon, Cross1Icon, TrashIcon } from "@radix-ui/react-icons";
+
+import { GearIcon, Cross1Icon } from "@radix-ui/react-icons";
 import { useToast } from "@/hooks/use-toast";
 
 const FamilyTreeComponent: React.FC = () => {
@@ -23,12 +24,11 @@ const FamilyTreeComponent: React.FC = () => {
   const [isAddFormOpen, setIsAddFormOpen] = useState(false);
   const [debugInfo, setDebugInfo] = useState<string>("");
   const { toast } = useToast();
-  const [isRemoveFormOpen, setIsRemoveFormOpen] = useState(false);
+
   const user = useFamilyStore((state) => state.user);
-  const [selectedRemoveMemberId, setSelectedRemoveMemberId] = useState<
-    string | null
-  >(null);
+
   const [selectedNode, setSelectedNode] = useState<string | null>(null);
+
   const [isSetting, setIsSetting] = useState(false);
 
   useEffect(() => {
@@ -46,40 +46,35 @@ const FamilyTreeComponent: React.FC = () => {
   useEffect(() => {
     if (divRef.current && nodes.length > 0 && !loading) {
       try {
-        if (
-          familyTreeRef.current &&
-          typeof familyTreeRef.current.destroy === "function"
-        ) {
+        if (familyTreeRef.current) {
           familyTreeRef.current.destroy();
-
           divRef.current.innerHTML = "";
         }
 
-        let newNode = nodes.map((node) => ({
-          id: node.id,
-          name: node.name ?? "",
-          gender: node.gender ?? "",
-          img: node.img,
-          fid: node.fid?.length > 0 ? node.fid[0] : undefined,
-          mid: node.mid?.length > 0 ? node.mid[0] : undefined,
-          pids: node.pids,
-        }));
-
-        FamilyTree.templates.myTemplate = Object.assign(
-          {},
-          FamilyTree.templates.tommy
-        );
-        FamilyTree.templates.myTemplate.node =
-          '<circle cx="50" cy="50" r="40" fill="#039BE5" stroke="#AEAEAE" stroke-width="1"></circle>' +
-          '<g style="cursor: pointer;" transform="matrix(0,0,0,0,100,100)" class="remove-btn">' +
-          '<circle cx="0" cy="0" r="12" fill="#FF0000" stroke="#FFFFFF" stroke-width="2"></circle>' +
-          "</g>";
-
-        FamilyTree.templates.myTemplate.nodeMenuButton = "";
+        const newNode = nodes.map((node) => {
+          const obj = {
+            id: node.id,
+            name: node.name ?? "",
+            gender: node.gender ?? "",
+          };
+          if (node.img) {
+            obj["img"] = node.img;
+          }
+          if (node.fid?.length > 0) {
+            obj["fid"] = node.fid;
+          }
+          if (node.mid?.length > 0) {
+            obj["mid"] = node.mid;
+          }
+          if (node.pids) {
+            obj["pids"] = node.pids;
+          }
+          return obj;
+        });
 
         const f = new FamilyTree(divRef.current, {
           nodes: newNode,
-          template: "rabin",
+          template: "atul",
           nodeBinding: {
             field_0: "name",
             img_0: "img",
@@ -90,21 +85,13 @@ const FamilyTreeComponent: React.FC = () => {
           toolbar: {
             zoom: true,
             fit: true,
-            expandAll: false,
           },
           roots: selectedNode ? [selectedNode] : [],
         });
 
-        f.on("click", function (sender, args) {
-          if (args.node && args.event.target.closest(".remove-btn")) {
-            handleRemove(args.node.id);
-            return false;
-          }
-        });
-
         familyTreeRef.current = f;
         familyTreeRef.current.onUpdateNode(handleEdit);
-        console.log(f);
+
         setDebugInfo("Family tree initialized successfully");
       } catch (err) {
         console.error("Error initializing family tree:", err);
@@ -113,7 +100,7 @@ const FamilyTreeComponent: React.FC = () => {
     }
   }, [nodes, loading]);
 
-  function showPermissionToast() {
+  function showPremissionToast() {
     toast({
       title: "Insufficient permissions",
       description: "please, contact admin",
@@ -121,48 +108,48 @@ const FamilyTreeComponent: React.FC = () => {
     });
   }
 
-  const handleEdit = async (args: any) => {
-    console.log("Edit node:", args);
-    try {
-      if (args.updateNodesData && args.updateNodesData.length > 0) {
-        const nodesToEdit = args.updateNodesData;
-        for (const node of nodesToEdit) {
-          try {
-            await updateFamilyMember(node.id, {
-              name: node.name,
-              gender: node.gender,
-              img: node.img,
-            });
-          } catch (error) {
-            if (error.code === "permission-denied") {
-              console.error("Insufficient permissions to update the document.");
-              showPermissionToast();
-            } else {
-              console.error("Error updating document: ", error);
-            }
-          });
-        });
-      }
+  type argsType = {
+    addNodesData: Array<object>;
+    updateNodesData: Array<{ [key: string]: string }>;
+    removeNodeId: number | string;
+  };
 
-    if(args.removeNodeId !== null) {
-      try{
+  const handleEdit = async (args: argsType) => {
+    console.log("Edit node:", args);
+
+    if (args.updateNodesData.length > 0) {
+      const nodesToEdit = args.updateNodesData;
+      nodesToEdit.forEach((node) => {
+        updateFamilyMember(node.id, {
+          name: node.name,
+          gender: node.gender,
+          img: node.img,
+        }).catch((error) => {
+          if (error.code == "permission-denied") {
+            console.error("Insufficient permissions to update the document.");
+            showPremissionToast();
+          } else {
+            console.error("Error updating document: ", error);
+          }
+        });
+      });
+    }
+
+    if (args.removeNodeId !== null) {
+      try {
         const s = await deleteFamilyMember(args.removeNodeId.toString());
         console.log("Document successfully removed!");
         console.log(s);
         return true;
+      } catch (error) {
+        if (error.code == "permission-denied") {
+          showPremissionToast();
+        } else {
+          console.error("Error removing document: ", error);
+        }
       }
-      catch(error){
-    if (error.code == "permission-denied") {
-      showPremissionToast()
-    } else {
-      console.error("Error removing document: ", error);
-      setDebugInfo(`Error removing member ${selectedRemoveMemberId}: ${error}`);
-      toast({
-        title: "Error",
-        description: "Failed to remove family member",
-        variant: "destructive",
-      });
     }
+    return false;
   };
 
   const handleAdd = () => {
@@ -186,14 +173,6 @@ const FamilyTreeComponent: React.FC = () => {
   const handleAddMember = (newMember: FamilyMember) => {
     setIsAddFormOpen(false);
     console.log("New member added:", newMember);
-    if (familyTreeRef.current) {
-      familyTreeRef.current.addNode(newMember);
-    }
-  };
-
-  const handleRemoveForm = () => {
-    console.log("Remove member form opened");
-    setIsRemoveFormOpen(true);
   };
 
   if (loading) {
@@ -212,62 +191,67 @@ const FamilyTreeComponent: React.FC = () => {
   return (
     <div className="flex flex-col md:flex-row w-full h-screen">
       <div
-        className={"w-full h-full " + (isSetting ? "md:w-3/4" : "")}
+        className={"w-full h-full" + (isSetting ? "md:w-3/4" : "")}
         style={{ height: "100vh" }}
         ref={divRef}
       ></div>
       {user && (
         <>
-        {isSetting ? (
-                <div className="w-full md:w-1/4 p-4 bg-gray-100">
-                  <div>
-                    <Button onClick={()=> setIsSetting(false)}>
-                      <Cross1Icon />
-                    </Button>
-                  </div>
-                <div className="mb-4">
-                  <label htmlFor="rootNodeSelect" className="block mb-2">
-                    Select Root Node:
-                  </label>
-                  <select
-                    id="rootNodeSelect"
-                    className="w-full p-2 border border-gray-300 rounded"
-                    onChange={(e) => {
-                      const selectedNodeId = e.target.value;
-                      setSelectedNode(selectedNodeId);
-                    }}
-                  >
-                    {nodes.map((node) => (
-                      <option
-                        key={node.id}
-                        value={node.id}
-                        selected={selectedNode === node.id}
-                      >
-                        {node.name}
-                      </option>
-                    ))}
-                  </select>
-                  <Button className="mb-4" onClick={setRootNode}>
-                    Set as Root Node
-                  </Button>
-                </div>
-                <Button onClick={handleAdd} className="mb-4">
-                  Add Member
+          {isSetting ? (
+            <div className="w-full md:w-1/4 p-4 bg-gray-100">
+              <div>
+                <Button onClick={() => setIsSetting(false)}>
+                  <Cross1Icon />
                 </Button>
-                {isAddFormOpen && (
-                  <AddMemberForm
-                    onSubmit={handleAddMember}
-                    onCancel={() => setIsAddFormOpen(false)}
-                    existingNodes={nodes}
-                  />
-                )}
-                {/* <DebugInfo info={debugInfo} nodeCount={nodes.length} /> */}
               </div>
-        ):(
-          <Button className="mt-7 mr-5 absolute top-14 ml-3" onClick={()=> setIsSetting(true)} variant="default" size="icon">
-            <GearIcon  />
-          </Button>
-        )}
+              <div className="mb-3 mt-5">
+                <label htmlFor="rootNodeSelect" className="block mb-2">
+                  Select Root Node:
+                </label>
+                <select
+                  id="rootNodeSelect"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  onChange={(e) => {
+                    const selectedNodeId = e.target.value;
+                    setSelectedNode(selectedNodeId);
+                  }}
+                >
+                  {nodes.map((node) => (
+                    <option
+                      key={node.id}
+                      value={node.id}
+                      selected={selectedNode === node.id}
+                    >
+                      {node.name}
+                    </option>
+                  ))}
+                </select>
+                <Button className="mb-4 mt-4" onClick={setRootNode}>
+                  Set as Root Node
+                </Button>
+              </div>
+              <Button onClick={handleAdd} className="mb-4">
+                Add Member
+              </Button>
+              {isAddFormOpen && (
+                <AddMemberForm
+                  onSubmit={handleAddMember}
+                  onCancel={() => setIsAddFormOpen(false)}
+                  existingNodes={nodes}
+                />
+              )}
+              {/* <DebugInfo info={debugInfo} nodeCount={nodes.length} /> */}
+            </div>
+          ) : (
+            <Button
+              className="mt-7 mr-5 m-3"
+              onClick={() => setIsSetting(true)}
+              variant="default"
+              size="icon"
+            >
+              <GearIcon />
+            </Button>
+          )}
         </>
       )}
     </div>
