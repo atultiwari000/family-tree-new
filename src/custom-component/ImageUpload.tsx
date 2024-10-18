b0bdf968fa44786660dcfe99ab8f8ef74ed15b13
EV-OD <evod599@gmail.com>
2024-10-18 14:16:35 +0545

    Refactor ImageUpload component and tsconfig.tsbuildinfo

--- src/custom-component/ImageUpload.tsx
+++ src/custom-component/ImageUpload.tsx
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const ImageUpload = ({ onUpload }) => {
-  const [imageUrl, setImageUrl] = useState("");
+// const ImageUpload = ({ onUpload }) => {
+//   const [imageUrl, setImageUrl] = useState("");
 
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const result = reader.result;
-        setImageUrl(result);
-        onUpload(result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
+//   const handleFileChange = (event) => {
+//     const file = event.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.onload = () => {
+//         const result = reader.result;
+//         setImageUrl(result);
+//         onUpload(result);
+//       };
+//       reader.readAsDataURL(file);
+//     }
+//   };
 
-  return (
-    <div>
-      <input type="file" accept="image/*" onChange={handleFileChange} />
-      {imageUrl && <img src={imageUrl} alt="Preview" width="100" />}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <input type="file" accept="image/*" onChange={handleFileChange} />
+//       {imageUrl && <img src={imageUrl} alt="Preview" width="100" />}
+//     </div>
+//   );
+// };
 
-export default ImageUpload;
+// export default ImageUpload;
